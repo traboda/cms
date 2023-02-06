c132742d055c444abd999e9658c6c85e764cef92
Ashwin S Shenoy <aswinshenoy65@gmail.com>
2023-02-06 17:18:28 +0530

    CORS fix

--- cms/settings.py
+++ cms/settings.py
@@ -28,14 +28,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
+
     'api',
     'attendance',
     'membership'
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
